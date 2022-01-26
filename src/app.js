f9cb37fc423f0965eba0da9c6cef94427becefe4
Adrián Molina Elvira <adrimo100@gmail.com>
2022-01-26 10:18:19 +0000

    Añadido impresión por pantalla de los dominios en index.html

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ["the", "our", "first", "last"];
+  let adjectives = ["great", "big", "small", "little"];
+  let nouns = ["jogger", "racoon", "mombasa", "voi"];
+  let extensions = [".com", ".es", ".io", ".org"];
+
+  for (let pron of pronouns)
+    for (let adj of adjectives)
+      for (let noun of nouns)
+        for (let ext of extensions) {
+          console.log(pron + adj + noun + ext);
+
+          document.querySelector(
+            "#domains"
+          ).innerHTML += `<div style = "font-weight: bold;
+          "> ${pron + adj + noun + ext} </div>`;
+        }
 };
